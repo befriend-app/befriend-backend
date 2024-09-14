f6f8ced47a65f6ba8678f9d51bf392fb0fbf6787
Eugene Gekhter <egekhter@gmail.com>
2024-09-14 13:33:28 -0500

    network secret keys cols

---
+++ migrations/20240914183117_network_secret_keys.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('networks_secret_keys', table => {
+        table.string('secret_key_from', 255).notNullable()
+            .after('is_active').alter();
+
+        table.string('secret_key_to', 255).notNullable()
+            .after('secret_key_from').alter();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
