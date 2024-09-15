14436532e848f96272f2d0edd4d0100d6db293a8
Eugene Gekhter <egekhter@gmail.com>
2024-09-14 19:24:51 -0500

    keys_exchanged col

---
+++ migrations/20240915002222_exchange_keys_col.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('networks', table => {
+        table.boolean('keys_exchanged').notNullable().defaultTo(false)
+            .after('priority');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
