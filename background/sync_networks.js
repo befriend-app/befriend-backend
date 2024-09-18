3f88c1cd48f21eaef6bc5170a913cdc6d3a7a9ef
Eugene Gekhter <egekhter@gmail.com>
2024-09-18 11:41:35 -0500

    sync networks only when self record exists

--- background/sync_networks.js
+++ background/sync_networks.js
@@ -22,89 +22,91 @@ loadScriptEnv();
 
             let my_network = await getNetworkSelf();
 
-            for(let domain of home_domains) {
-                try {
-                    let r = await axios.get(getURL(domain, `networks`));
+            if(my_network) {
+                for(let domain of home_domains) {
+                    try {
+                        let r = await axios.get(getURL(domain, `networks`));
 
-                    if(r.data && r.data.networks) {
-                        for(let network of r.data.networks) {
-                            //do not do anything if network belongs to me
-                            if(my_network.network_token === network.network_token) {
-                                continue;
-                            }
-
-                            let keys_exchanged = false;
-
-                            //add to db if not exists
-                            try {
-                                let db_qry = await conn('networks')
-                                    .where('network_token', network.network_token)
-                                    .first();
-
-                                if(!db_qry) {
-                                    let network_insert = {};
-
-                                    //prepare data insert based on networks table cols
-                                    for(let col of cols) {
-                                        if(col in network) {
-                                            network_insert[col] = network[col];
-                                        }
-                                    }
-
-                                    network_insert.is_self = false;
-                                    network_insert.keys_exchanged = false;
-                                    network_insert.created = timeNow();
-                                    network_insert.updated = timeNow();
-
-                                    await conn('networks')
-                                        .insert(network_insert);
-                                } else {
-                                    keys_exchanged = db_qry.keys_exchanged;
-                                }
-                            } catch(e) {
-                                console.error(e);
-                            }
-
-                            //exchange keys if needed
-                            if(!keys_exchanged) {
-                                //do not initiate process if my network was added after this network to prevent duplicate cross-send
-                                let my_network_created = null;
-                                let their_network_created = network.created;
-
-                                for(let _network of r.data.networks) {
-                                    if(_network.network_token === my_network.network_token) {
-                                        my_network_created = _network.created;
-                                        break;
-                                    }
-                                }
-
-                                if(my_network_created > their_network_created) {
+                        if(r.data && r.data.networks) {
+                            for(let network of r.data.networks) {
+                                //do not do anything if network belongs to me
+                                if(my_network.network_token === network.network_token) {
                                     continue;
                                 }
 
-                                //way to know communicating with non-spoofed network
+                                let keys_exchanged = false;
 
-                                //befriend_network_token
-                                let befriend_qry = await conn('networks')
-                                    .where('')
+                                //add to db if not exists
+                                try {
+                                    let db_qry = await conn('networks')
+                                        .where('network_token', network.network_token)
+                                        .first();
 
-                                //self
-                                //sending_network_token
+                                    if(!db_qry) {
+                                        let network_insert = {};
 
-                                //receiving_network_token
-                                //to_network_token
+                                        //prepare data insert based on networks table cols
+                                        for(let col of cols) {
+                                            if(col in network) {
+                                                network_insert[col] = network[col];
+                                            }
+                                        }
 
-                                //encrypt self_network_token with befriend_secret_key for to_network
-                                //decrypt encrypted self_network_token on to_network,
-                                // if value matches self_network_token, begin key exchange process
-                                debugger;
+                                        network_insert.is_self = false;
+                                        network_insert.keys_exchanged = false;
+                                        network_insert.created = timeNow();
+                                        network_insert.updated = timeNow();
+
+                                        await conn('networks')
+                                            .insert(network_insert);
+                                    } else {
+                                        keys_exchanged = db_qry.keys_exchanged;
+                                    }
+                                } catch(e) {
+                                    console.error(e);
+                                }
+
+                                //exchange keys if needed
+                                if(!keys_exchanged) {
+                                    //do not initiate process if my network was added after this network to prevent duplicate cross-send
+                                    let my_network_created = null;
+                                    let their_network_created = network.created;
+
+                                    for(let _network of r.data.networks) {
+                                        if(_network.network_token === my_network.network_token) {
+                                            my_network_created = _network.created;
+                                            break;
+                                        }
+                                    }
+
+                                    if(my_network_created > their_network_created) {
+                                        continue;
+                                    }
+
+                                    //way to know communicating with non-spoofed network
+
+                                    //befriend_network_token
+                                    let befriend_qry = await conn('networks')
+                                        .where('')
+
+                                    //self
+                                    //sending_network_token
+
+                                    //receiving_network_token
+                                    //to_network_token
+
+                                    //encrypt self_network_token with befriend_secret_key for to_network
+                                    //decrypt encrypted self_network_token on to_network,
+                                    // if value matches self_network_token, begin key exchange process
+                                    debugger;
+                                }
                             }
-                        }
 
-                        break;
+                            break;
+                        }
+                    } catch(e) {
+                        console.error(e);
                     }
-                } catch(e) {
-                    console.error(e);
                 }
             }
         } catch(e) {
