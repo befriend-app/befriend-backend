9e0990dd59a536f4fcd0cf7c3f5ca43eb5bdbecd
Eugene Gekhter <egekhter@gmail.com>
2024-09-19 16:11:40 -0500

    sync table

---
+++ migrations/20240919210518_sync_table.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('sync', table => {
+        table.bigIncrements('id').unsigned().primary();
+        table.string('sync_process', 255).notNullable();
+        table.bigInteger('network_id').unsigned().notNullable();
+        table.bigInteger('last_updated').nullable();
+
+        table.bigInteger('created').notNullable();
+        table.bigInteger('updated').notNullable();
+
+        table.foreign('network_id').references('id').inTable('networks');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
