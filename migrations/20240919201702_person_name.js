9bba24b8e5876fa8fb6192ec8576b4092d8f9619
Eugene Gekhter <egekhter@gmail.com>
2024-09-19 15:18:35 -0500

    from person_name to first_name, last_name

---
+++ migrations/20240919201702_person_name.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('persons', table => {
+        table.string('first_name').nullable()
+            .after('network_id');
+
+        table.string('last_name').nullable()
+            .after('first_name');
+
+        table.dropColumn('person_name');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
