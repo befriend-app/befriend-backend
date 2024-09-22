f396fe49db4ae6d5d59de8288dff45b7b15bf7fc
Eugene Gekhter <egekhter@gmail.com>
2024-09-22 11:13:11 -0500

    fix cancelled_at default value

---
+++ migrations/20240922161123_fix_cancelled_at_default.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('activities_persons', table => {
+        table.integer('cancelled_at').nullable()
+            .alter();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
