72fd3cfac282452b705122fab9e40269df586c80
Eugene Gekhter <egekhter@gmail.com>
2024-10-14 18:31:42 -0500

    index fsq_place_id

---
+++ migrations/20241014232954_index_fsq_place_id.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('places', table => {
+        table.index('fsq_place_id')
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
