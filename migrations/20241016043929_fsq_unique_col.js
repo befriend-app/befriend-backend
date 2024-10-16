8984ba338cb345381d7106219d37e5bf737363e2
Eugene Gekhter <egekhter@gmail.com>
2024-10-15 23:42:31 -0500

    unique fsq_place_id col

---
+++ migrations/20241016043929_fsq_unique_col.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('places', table => {
+        table.string('fsq_place_id').notNullable().unique().alter();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
