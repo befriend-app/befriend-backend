ff0a8523501c5d6c87a45cf8bc61274bdae484bc
egekhter <egekhter@gmail.com>
2024-10-27 21:28:39 -0500

    user devices migration

---
+++ migrations/20241028021801_user_devices.js
@@ -0,0 +1,29 @@
+let table_name = 'persons_devices';
+
+exports.up = function(knex) {
+    return knex.schema.createTable(table_name, (table) => {
+        table.increments('id').primary();
+        table.bigInteger('person_id').unsigned().notNullable();
+
+        table.string('token', 255).notNullable();
+
+        table.enum('platform', ['ios', 'android']).notNullable();
+
+        table.boolean('is_current').defaultTo(false);
+
+        table.bigInteger('created').notNullable();
+        table.bigInteger('updated').notNullable();
+
+        // Indexes
+        table.index('person_id');
+        table.index('token');
+        table.index('platform');
+        table.index('is_current');
+
+        table.foreign('person_id').references('id').inTable('persons');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists(table_name);
+};
