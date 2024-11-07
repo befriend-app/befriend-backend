9ed9a4f777bfbc43edde8f337a14df191e5403a1
egekhter <egekhter@gmail.com>
2024-11-07 01:23:25 -0600

    nullable sync network_id

---
+++ migrations/20241107072203_nullable.js
@@ -0,0 +1,16 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    return knex.schema.alterTable('sync', (table) => {
+        table.bigInteger('network_id').unsigned().nullable().alter();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+};
