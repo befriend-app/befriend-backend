6c6a89c2a45f38d315afbf82a8f5da8b27ee9b83
egekhter <egekhter@gmail.com>
2024-11-07 00:34:50 -0600

    city/state timestamps

---
+++ migrations/20241107063208_states_timestamps.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    return knex.schema.alterTable('open_states', (table) => {
+        table.bigInteger('created').notNullable();
+        table.bigInteger('updated').notNullable();
+        table.bigInteger('deleted').nullable();
+    }).alterTable('open_cities', (table) => {
+        table.bigInteger('created').notNullable();
+        table.bigInteger('updated').notNullable();
+        table.bigInteger('deleted').nullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.alterTable('open_states', (table) => {
+        table.dropColumn('created');
+        table.dropColumn('updated');
+        table.dropColumn('deleted');
+    }).alterTable('open_cities', (table) => {
+        table.dropColumn('created');
+        table.dropColumn('updated');
+        table.dropColumn('deleted');
+    });
+};
