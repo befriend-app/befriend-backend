508d1170d5b2174a67ece5de45cbfa66259f93ae
egekhter <egekhter@gmail.com>
2024-11-10 12:32:45 -0600

    city/state token

---
+++ migrations/20241110183119_city_state_token.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .alterTable('open_cities', (table) => {
+            table.string('token').notNullable().after('id');
+        }).alterTable('open_states', (table) => {
+            table.string('token').notNullable().after('id');
+        });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema
+        .alterTable('open_cities', (table) => {
+            table.dropColumn('token');
+        }).alterTable('open_states', (table) => {
+            table.dropColumn('token');
+        });
+};
