ef6b7a49f2a821fd6fbe1a638cdc4be935353a85
egekhter <egekhter@gmail.com>
2024-11-13 17:22:39 -0600

    apple to music brain genre map

---
+++ migrations/20241113232115_apple_mb_genre_map.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .alterTable('music_genres', (table) => {
+            table.string('mb_genres').nullable().after('apple_id');
+        });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema
+        .alterTable('music_genres', (table) => {
+            table.dropColumn('mb_genres');
+        });
+};
