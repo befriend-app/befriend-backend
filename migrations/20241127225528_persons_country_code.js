183962656fe151481d2877f0b66744b251393999
egekhter <egekhter@gmail.com>
2024-11-27 16:56:34 -0600

    persons country code

---
+++ migrations/20241127225528_persons_country_code.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.alterTable('persons', (table) => {
+        table.string('country_code', 10).nullable().after('password');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.alterTable('persons', (table) => {
+        table.dropColumn('country_code');
+    });
+};
