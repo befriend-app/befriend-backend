e2fdaf3edeb2418d8aac146471acc71ff314770e
egekhter <egekhter@gmail.com>
2024-12-13 18:32:42 -0600

    networks person count col

--- migrations/20241213231943_network_active.js
+++ migrations/20241213231943_network_active.js
@@ -5,6 +5,7 @@
 exports.up = async function (knex) {
     await knex.schema.alterTable('networks', (table) => {
         table.boolean('is_active').defaultTo(false).notNullable().after('is_online');
+        table.bigInteger('persons_count').defaultTo(0).notNullable().after('keys_exchanged');
     });
 };
 
@@ -15,5 +16,6 @@ exports.up = async function (knex) {
 exports.down = async function (knex) {
     await knex.schema.alterTable('networks', (table) => {
         table.dropColumn('is_active');
+        table.dropColumn('persons_count');
     });
 };
