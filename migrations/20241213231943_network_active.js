53a8e1ff6d4d6177cb134eaba59b48e4f8c58d52
egekhter <egekhter@gmail.com>
2024-12-13 18:52:14 -0600

    persons networks deleted col

--- migrations/20241213231943_network_active.js
+++ migrations/20241213231943_network_active.js
@@ -7,6 +7,10 @@ exports.up = async function (knex) {
         table.boolean('is_active').defaultTo(false).notNullable().after('is_online');
         table.bigInteger('persons_count').defaultTo(0).notNullable().after('keys_exchanged');
     });
+
+    await knex.schema.alterTable('persons_networks', (table) => {
+        table.bigInteger('deleted').nullable().after('updated');
+    });
 };
 
 /**
@@ -14,6 +18,10 @@ exports.up = async function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
+    await knex.schema.alterTable('persons_networks', (table) => {
+        table.dropColumn('deleted');
+    });
+
     await knex.schema.alterTable('networks', (table) => {
         table.dropColumn('is_active');
         table.dropColumn('persons_count');
