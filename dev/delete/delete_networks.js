89719f3aebd2116217ec7cccce518370d6d01101
egekhter <egekhter@gmail.com>
2024-12-16 16:28:45 -0600

    delete networks dev script

---
+++ dev/delete/delete_networks.js
@@ -0,0 +1,89 @@
+const { loadScriptEnv, isProdApp } = require('../../services/shared');
+const { keys, deleteKeys } = require('../../services/cache');
+
+loadScriptEnv();
+
+function main(is_me) {
+    return new Promise(async (resolve, reject) => {
+        console.log('Delete: networks');
+
+        if (isProdApp()) {
+            console.error('App env: [prod]', 'exiting');
+            return resolve();
+        }
+
+        let dbs = [process.env.DB_NAME, 'befriend-4001', 'befriend-4002'];
+
+        for (let db of dbs) {
+            let connection = {
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: db,
+            };
+
+            if (process.env.DB_PORT) {
+                connection.port = parseInt(process.env.DB_PORT);
+            }
+
+            let knex = require('knex')({
+                client: process.env.DB_CLIENT,
+                connection: connection,
+            });
+
+            let bulk_delete_count = 50000;
+
+            //delete activities
+            let tables = [
+                'persons_filters_networks',
+                'persons_networks',
+                'networks_secret_keys',
+                'networks',
+            ];
+
+            await knex('networks')
+                .update({
+                    registration_network_id: null
+                });
+
+            for (let table of tables) {
+                while (true) {
+                    try {
+                        var pn_qry = await knex(table)
+                            .select('id')
+                            .limit(bulk_delete_count);
+                    } catch (e) {
+                        break;
+                    }
+
+                    if (!pn_qry.length) {
+                        break;
+                    } else {
+                        let ids = pn_qry.map((x) => x.id);
+
+                        await knex(table).whereIn('id', ids).delete();
+                    }
+                }
+            }
+        }
+
+        await deleteKeys([keys.networks, keys.networks_filters]);
+
+        resolve();
+    });
+}
+
+module.exports = {
+    main: main,
+};
+
+if (require.main === module) {
+    (async function () {
+        try {
+            await main(true);
+            process.exit();
+        } catch (e) {
+            console.error(e);
+        }
+    })();
+}
