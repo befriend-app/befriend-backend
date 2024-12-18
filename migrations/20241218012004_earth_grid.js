8b3ede1cf723bfb32c32fe693f1108f4bd5c0fc1
egekhter <egekhter@gmail.com>
2024-12-17 19:26:46 -0600

    earth grid migration

---
+++ migrations/20241218012004_earth_grid.js
@@ -0,0 +1,41 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    await knex.schema.createTable('earth_grid', (table) => {
+        table.increments('id').primary();
+
+        table.string('token', 32).notNullable();
+        table.integer('lat_key').notNullable();
+        table.integer('lon_key').notNullable();
+        table.decimal('center_lat', 7, 3);
+        table.decimal('center_lon', 7, 3);
+        table.decimal('grid_size_km', 5,1);
+
+        table.bigInteger('created').notNullable();
+        table.bigInteger('updated').notNullable();
+        table.bigInteger('deleted').nullable();
+
+        table.index('token');
+        table.index('lat_key');
+        table.index('lon_key');
+    });
+
+    await knex.schema.alterTable('persons', (table) => {
+        table.integer('grid_id').unsigned().nullable().after('network_id').references('id').inTable('earth_grid');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.alterTable('persons', (table) => {
+        table.dropForeign('grid_id');
+        table.dropColumn('grid_id');
+    });
+
+    await knex.schema.dropTableIfExists('earth_grid');
+};
