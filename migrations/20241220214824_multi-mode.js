e01a3a7c42a2adad6529f3945e048aa6c6d3e7cd
egekhter <egekhter@gmail.com>
2024-12-20 15:53:22 -0600

    multiple modes migration

---
+++ migrations/20241220214824_multi-mode.js
@@ -0,0 +1,42 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    const hasColumn = await knex.schema.hasColumn('persons', 'mode_id');
+
+    if (hasColumn) {
+        await knex.schema.alterTable('persons', (table) => {
+            table.renameColumn('mode_id', 'current_mode_id');
+        });
+    }
+
+    await knex.schema.alterTable('persons', (table) => {
+        table.string('modes').nullable();
+    });
+
+    await knex.schema.alterTable('activities', (table) => {
+        table
+            .integer('mode_id')
+            .unsigned()
+            .notNullable()
+            .after('activity_type_id')
+            .references('id')
+            .inTable('modes');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.alterTable('persons', (table) => {
+        table.dropColumn('modes');
+    });
+
+    await knex.schema.alterTable('activities', (table) => {
+        table.dropForeign('mode_id');
+        table.dropColumn('mode_id');
+    });
+};
