f686323ad02bea65a1624ee9b4c8c308062edfa5
egekhter <egekhter@gmail.com>
2024-12-26 11:40:40 -0600

    persons timezone col

---
+++ migrations/20241226173822_persons_timezone.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    await knex.schema.alterTable('persons', (table) => {
+        table.string('timezone').nullable().after('location_lon_1000');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.alterTable('persons', (table) => {
+        table.dropColumn('timezone');
+    });
+};
