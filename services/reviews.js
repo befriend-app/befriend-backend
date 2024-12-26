f31f11ee75ddcf29000ddf91c4186cfd55f5384a
egekhter <egekhter@gmail.com>
2024-12-26 17:09:01 -0600

    init reviews service

--- services/reviews.js
+++ services/reviews.js
@@ -1,3 +1,29 @@
-module.exports = {
+let dbService = require('../services/db');
 
+function getReviews() {
+    return new Promise(async (resolve, reject) => {
+        if(module.exports.data) {
+            return resolve(module.exports.data);
+        }
+
+        try {
+            let conn = await dbService.conn();
+
+            let data = await conn('reviews')
+                .where('is_active', true)
+                .orderBy('sort_position');
+
+            module.exports.data = data;
+
+            return resolve(data);
+        } catch(e) {
+            console.error(e);
+            return reject();
+        }
+    });
+}
+
+module.exports = {
+    data: null,
+    getReviews
 };
