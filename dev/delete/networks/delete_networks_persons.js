344846db6ed91089b087321b40ba3b246339ce39
egekhter <egekhter@gmail.com>
2025-01-17 19:43:36 -0600

    update delete networks persons

--- dev/delete/networks/delete_networks_persons.js
+++ dev/delete/networks/delete_networks_persons.js
@@ -1,6 +1,7 @@
+const cacheService = require('../../../services/cache');
 const { loadScriptEnv, isProdApp } = require('../../../services/shared');
 const { keys: systemKeys } = require('../../../services/system');
-const { getKeysWithPrefix, deleteKeys } = require('../../../services/cache');
+const { getGridLookup } = require('../../../services/grid');
 
 loadScriptEnv();
 
@@ -44,18 +45,73 @@ function main() {
 
             let persons = await knex('persons')
                 .whereNot('network_id', network_self.id)
-                .select('id');
+                .select('id', 'person_token', 'grid_id');
+
+            let gridLookup = await getGridLookup();
 
             for (let i = 0; i < persons.length; i += bulk_delete_count) {
                 let chunk = persons.slice(i, i + bulk_delete_count);
 
                 let ids = chunk.map(x => x.id);
+                let tokens = chunk.map(x => x.token);
+                let grids = {};
 
-                await knex('persons').whereIn('id', ids).delete();
+                for(let p of chunk) {
+                    let grid = gridLookup.byId[p.grid_id];
+
+                    if(grid) {
+                        grids[grid.token] = true;
+                    }
+                }
+
+                await knex('persons_partner')
+                    .whereIn('person_id', ids)
+                    .delete();
+
+                await knex('persons_kids')
+                    .whereIn('person_id', ids)
+                    .delete();
+
+                await knex('persons')
+                    .whereIn('id', ids)
+                    .delete();
+
+                let pipeline = cacheService.startPipeline();
+
+                //clean up grid sets
+                for(let grid in grids) {
+                    let sorted_keys = await cacheService.getKeysWithPrefix(cacheService.keys.persons_grid_sorted(grid, ''));
+                    let set_keys = await cacheService.getKeysWithPrefix(cacheService.keys.persons_grid_set(grid, ''));
+                    let exclude_keys = await cacheService.getKeysWithPrefix(`persons:grid:${grid}:exclude`);
+                    let send_receive = await cacheService.getKeysWithPrefix(cacheService.keys.persons_grid_send_receive(grid, 'verifications'));
+
+                    let srem_keys = set_keys.concat(exclude_keys).concat(send_receive);
+
+                    for(let token of tokens) {
+                        for(let key of srem_keys) {
+                            pipeline.sRem(key, token);
+                        }
+
+                        for(let key of sorted_keys) {
+                            pipeline.zRem(key, token);
+                        }
+                    }
+                }
+
+                for(let token of tokens) {
+                    pipeline.del(cacheService.keys.activities(token));
+                    pipeline.del(cacheService.keys.person(token));
+                    pipeline.del(cacheService.keys.persons_activities(token));
+                    pipeline.del(cacheService.keys.person_filters(token));
+                    pipeline.del(cacheService.keys.person_sections(token));
+                }
+
+                await cacheService.execPipeline(pipeline);
             }
 
             try {
                 await knex('sync').where('sync_process', systemKeys.sync.network.persons).delete();
+                await knex('sync').where('sync_process', systemKeys.sync.network.persons_modes).delete();
             } catch (e) {
                 console.error(e);
             }
