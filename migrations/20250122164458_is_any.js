bdac0a48ab041aa07163e7dca545d6e9155c4995
egekhter <egekhter@gmail.com>
2025-01-22 11:17:56 -0600

    is any column

---
+++ migrations/20250122164458_is_any.js
@@ -0,0 +1,11 @@
+exports.up = async function(knex) {
+    await knex.schema.alterTable('persons_filters', (table) => {
+        table.boolean('is_any').notNullable().defaultTo(false).after('is_negative');
+    });
+};
+
+exports.down = async function(knex) {
+    await knex.schema.alterTable('persons_filters', (table) => {
+        table.dropColumn('is_any');
+    });
+};
