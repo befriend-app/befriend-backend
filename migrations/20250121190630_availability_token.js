fb2d94cf73054c2dbb7d1ba4e8429b2547502bff
egekhter <egekhter@gmail.com>
2025-01-21 20:23:55 -0600

    token on persons_filters

--- migrations/20250121190630_availability_token.js
+++ migrations/20250121190630_availability_token.js
@@ -1,10 +1,18 @@
 exports.up = async function(knex) {
+    await knex.schema.alterTable('persons_filters', (table) => {
+        table.string('token').notNullable().after('person_id');
+    });
+
     await knex.schema.alterTable('persons_availability', (table) => {
         table.string('token').notNullable().after('person_id');
     });
 };
 
 exports.down = async function(knex) {
+    await knex.schema.alterTable('persons_filters', (table) => {
+        table.dropColumn('token');
+    });
+
     await knex.schema.alterTable('persons_availability', (table) => {
         table.dropColumn('token');
     });
