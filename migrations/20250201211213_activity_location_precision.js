9c8a8a6d9cffdc2420376fa8cdff9f38e704a53a
egekhter <egekhter@gmail.com>
2025-02-01 16:15:20 -0600

    activity location precision

---
+++ migrations/20250201211213_activity_location_precision.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.alterTable('activities', (table) => {
+        table.float('location_lat', 10, 4).notNullable().alter();
+        table.float('location_lon', 10, 4).notNullable().alter();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
