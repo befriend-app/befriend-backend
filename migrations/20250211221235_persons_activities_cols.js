5c0e7954f1c3c91d3fb46c59d39683ff7bbdd76c
egekhter <egekhter@gmail.com>
2025-02-11 16:35:12 -0600

    access token index

--- migrations/20250211221235_persons_activities_cols.js
+++ migrations/20250211221235_persons_activities_cols.js
@@ -7,6 +7,8 @@ exports.up = async function(knex) {
         table.string('access_token').nullable().after('left_at');
         table.string('first_name').nullable().after('access_token');
         table.string('image_url').nullable().after('first_name');
+
+        table.index('access_token')
     });
 };
 
@@ -16,6 +18,8 @@ exports.up = async function(knex) {
  */
 exports.down = async function(knex) {
     await knex.schema.alterTable('activities_persons', (table) => {
+        table.dropIndex('access_token');
+
         table.dropColumn('access_token');
         table.dropColumn('first_name');
         table.dropColumn('image_url');
