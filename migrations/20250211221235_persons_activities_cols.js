bdd2d7d5e6d50473b0c10feda4be8919e9029cba
egekhter <egekhter@gmail.com>
2025-02-11 16:14:33 -0600

    persons activities cols

---
+++ migrations/20250211221235_persons_activities_cols.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.alterTable('activities_persons', (table) => {
+        table.string('access_token').nullable().after('left_at');
+        table.string('first_name').nullable().after('access_token');
+        table.string('image_url').nullable().after('first_name');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema.alterTable('activities_persons', (table) => {
+        table.dropColumn('access_token');
+        table.dropColumn('first_name');
+        table.dropColumn('image_url');
+    });
+};
