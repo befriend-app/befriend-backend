1efbf08c9ba39b8ea1f1d9bb66b87c934e0e756a
egekhter <egekhter@gmail.com>
2025-03-02 16:58:29 -0600

    no show percent col

---
+++ migrations/20250302225557_no_show_percent.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    await knex.schema.alterTable('persons', (table) => {
+        table.decimal('no_show_percent').defaultTo(0).after('rating_fun');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.alterTable('persons', (table) => {
+        table.dropColumn('no_show_percent');
+    });
+};
