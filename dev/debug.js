7f13c880b784a05c279e8755c96e6f6d7e2299f9
egekhter <egekhter@gmail.com>
2025-05-23 20:08:38 -0500

    debug settings

--- dev/debug.js
+++ dev/debug.js
@@ -13,7 +13,7 @@ module.exports = {
     matching: {
         logs: isProdApp() ? false : true,
         get_matches: isProdApp() ? false : true,
-        filter_matches: isProdApp() ? false : true,
+        filter_matches: isProdApp() ? false : false,
         send_count: 3,
         filters: [
             //notifications
